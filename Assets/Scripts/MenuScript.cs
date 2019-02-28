58499eb25b8d5f4ccdf7c0886cf014da6cac9a96
wx31179 <orange_wrj@hotmail.com>
2019-02-28 10:13:29 +0800

    删除.meta文件和projects文件夹

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Stage1");
+        // "Stage1" is the name of the first scene we created.
+        //Application.LoadLevel("Stage1");
+
+    }
+}
