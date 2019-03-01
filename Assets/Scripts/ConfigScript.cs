1b29eed2b69decc05433b9efe974afeed6e3ff7a
wx31179-work <orange_wrj@hotmail.com>
2019-03-01 17:35:46 +0800

    添加了ConfigScript脚本

---
+++ Assets/Scripts/ConfigScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConfigScript : MonoBehaviour
+{
+    public bool music = true;
+    private static bool haveConfig = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!haveConfig)
+        {
+            GameObject config = GameObject.Find("script");
+            Debug.Log(config.GetComponent<ConfigScript>().music);
+            haveConfig = true;
+            GameOverScript.DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            music = true;
+            Debug.Log(music);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TurnOffMusic()
+    {
+        if (music)
+        {
+            music = false;
+        }
+        else
+        {
+            music = true;
+        }
+        
+
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+
+}
